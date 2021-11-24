b82aaa760e1c9c187cae4caa3f3e831801a29546
mkmwangi <morrismwangi26@gmail.com>
2021-11-24 18:06:15 +0300

    Initial Commit

---
+++ CIB_Absa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CIB_Absa
+//
+//  Created by Morris Mwangi on 24/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
